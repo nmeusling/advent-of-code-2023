dedc59bb556dbd3acba9742e59ae56622b4ee4f5
Natalie Menato <15682036+nmeusling@users.noreply.github.com>
2023-12-01 18:40:33 -0500

    solution for part 2

--- day1.py
+++ day1.py
@@ -10,14 +10,66 @@ def extract_digits(text: str):
             digits += char
     return digits
 
-def get_calibration_value(text: str):
+def replace_first_and_last_written_numbers(text: str):
+    digit_replacements = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+    # replace first word digit
+    updated_text = text
+    for i in range(len(text)):
+        to_replace = None
+        substring = text[i:i+3]
+        if substring in digit_replacements:
+            to_replace = substring
+        substring = text[i:i+4]
+        if substring in digit_replacements:
+            to_replace = substring
+        substring = text[i:i+5]
+        if substring in digit_replacements:
+            to_replace = substring
+        
+        if to_replace:
+            updated_text = text.replace(to_replace, digit_replacements[to_replace])
+            break
+     
+
+    # replace last word digit
+    final_text = updated_text
+    for i in range(len(updated_text)-1, -1, -1):
+        to_replace = None
+        substring = updated_text[i-3+1:i+1]
+        if substring in digit_replacements:
+            to_replace = substring
+        substring = updated_text[i-4+1:i+1]
+        if substring in digit_replacements:
+            to_replace = substring
+        substring = updated_text[i-5+1:i+1]
+        if substring in digit_replacements:
+            to_replace = substring
+            
+        if to_replace:
+            final_text = updated_text.replace(to_replace, digit_replacements[to_replace])
+            break
+    return final_text
+
+def get_calibration_value(text: str, replace_digits: bool = False):
+    if replace_digits:
+        text = replace_first_and_last_written_numbers(text)
     digits = extract_digits(text)
     return int(digits[0]+digits[-1])
 
-def sum_all_calibration_values(text_values: list):
+def sum_all_calibration_values(text_values: list, replace_digits: bool = False):
     sum = 0
     for text in text_values:
-        calibration_value = get_calibration_value(text)
+        calibration_value = get_calibration_value(text, replace_digits)
         sum += calibration_value
     return sum
 
@@ -25,5 +77,10 @@ def solve_day_1_part_1():
     text_values = get_input()
     return sum_all_calibration_values(text_values)
 
+def solve_day_1_part_2():
+    text_values = get_input()
+    return sum_all_calibration_values(text_values, replace_digits=True)
+
 if __name__ == '__main__':
     print(f"Part 1: {solve_day_1_part_1()}")
+    print(f"Part 2: {solve_day_1_part_2()}")
--- test_day1.py
+++ test_day1.py
@@ -1,4 +1,4 @@
-from day1 import extract_digits, get_calibration_value, sum_all_calibration_values
+from day1 import extract_digits, get_calibration_value, sum_all_calibration_values, replace_first_and_last_written_numbers
 
 def test_extract_digits():
     assert(extract_digits("1abc2")) == "12"
@@ -15,3 +15,17 @@ def test_get_calibration_value():
 def test_sum_all_calibration_values():
     text_values = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
     assert sum_all_calibration_values(text_values) == 142
+
+def test_sum_all_calibration_values_with_replacement():
+    text_values = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
+    assert sum_all_calibration_values(text_values, replace_digits=True) == 281
+
+def test_replace_written_numbers():
+    assert replace_first_and_last_written_numbers("two1nine") == "219"
+    assert replace_first_and_last_written_numbers("eightwothree") == "8wo3"
+    assert replace_first_and_last_written_numbers("abcone2threexyz") == "abc123xyz"
+    assert replace_first_and_last_written_numbers("xtwone3four") == "x2ne34"
+    assert replace_first_and_last_written_numbers("4nineeightseven2") == "49eight72"
+    assert replace_first_and_last_written_numbers("zoneight234") == "z1ight234"
+    assert replace_first_and_last_written_numbers("7pqrstsixteen") == "7pqrst6teen"
+    assert replace_first_and_last_written_numbers("342dfafdsfead") == "342dfafdsfead"
