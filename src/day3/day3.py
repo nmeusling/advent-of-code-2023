6034ae584cd0e437407c4ec5b3d8cfa21acb16e2
Natalie Menato <15682036+nmeusling@users.noreply.github.com>
2023-12-04 22:02:32 -0500

    solution for day 3 part 2

--- src/day3/day3.py
+++ src/day3/day3.py
@@ -9,7 +9,7 @@ def is_special_character(c):
 
 
 def is_part_number(length, start_row, start_column, schematic):
-    end_column = length + start_column -1
+    end_column = length + start_column - 1
     end_row = start_row
     if start_row > 0:
         start_row = start_row - 1
@@ -28,7 +28,7 @@ def is_part_number(length, start_row, start_column, schematic):
 
 
 def get_ints_from_line(line: str):
-    i=0
+    i = 0
     found_numbers = []
     is_number = False
     length = 1
@@ -54,17 +54,75 @@ def get_ints_from_line(line: str):
     return found_numbers
 
 
+def get_adjacent_parts(row, column, schematic):
+    start_row = row
+    end_row = row
+    start_column = column
+    end_column = column
+    if row > 0:
+        start_row = row - 1
+    if row < len(schematic) - 1:
+        end_row = row + 1
+    if column > 0:
+        start_column = column - 1
+    if column < len(schematic[0]) - 1:
+        end_column = column + 1
+
+    part_numbers = []
+
+    for i in range(start_row, end_row + 1):
+        j = start_column
+        while j < end_column + 1:
+            if schematic[i][j].isdigit():
+                part_number, skip = get_number_touching_index(schematic[i], j)
+                part_numbers.append(part_number)
+                j += skip
+            j += 1
+
+    return part_numbers
+
+
+def get_number_touching_index(row, index):
+    is_number = row[index].isdigit()
+    pointer = index
+    while is_number and pointer > 0:
+        is_number = row[pointer - 1].isdigit()
+        if is_number:
+            pointer = pointer - 1
+    start_index = pointer
+
+    is_number = row[index].isdigit()
+    while is_number and pointer < len(row) - 1:
+        is_number = row[pointer+1].isdigit()
+        if is_number:
+            pointer = pointer + 1
+    end_index = pointer
+
+    return row[start_index:end_index+1], end_index - index
+
+
 def solve_day3_part1(schematic):
     sum = 0
     for row_number, row_contents in enumerate(schematic):
         numbers = get_ints_from_line(row_contents)
         for start, length in numbers:
             if is_part_number(length, row_number, start, schematic):
-                sum += int(row_contents[start:start+length])
+                sum += int(row_contents[start:start + length])
 
     return sum
 
 
+def solve_day3_part2(schematic):
+    sum = 0
+    for row_number, row_contents in enumerate(schematic):
+        for column_number, char in enumerate(row_contents):
+            if char == '*':
+                parts = get_adjacent_parts(row_number, column_number, schematic)
+                if len(parts) == 2:
+                    sum += int(parts[0]) * int(parts[1])
+    return sum
+
 if __name__ == '__main__':
     input = get_input()
     print(f"Part 1: {solve_day3_part1(input)}")
+    print(f"Part 1: {solve_day3_part2(input)}")
--- test/test_day3.py
+++ test/test_day3.py
@@ -1,4 +1,5 @@
-from src.day3.day3 import is_part_number, get_ints_from_line, solve_day3_part1
+from src.day3.day3 import is_part_number, get_ints_from_line, solve_day3_part1, get_adjacent_parts, \
+    get_number_touching_index, solve_day3_part2
 
 
 def test_is_part_number():
@@ -49,3 +50,48 @@ def test_day3_part1():
                  '.664.598..',
                  '......-598',]
     assert solve_day3_part1(schematic) == 4361 + 598
+
+
+def test_get_number_touching_index():
+    assert get_number_touching_index('467..114..', 1) == ('467', 1)
+    assert get_number_touching_index('467..114..', 0) == ('467', 2)
+    assert get_number_touching_index('467..114..', 2) == ('467', 0)
+    assert get_number_touching_index('467..114..', 5) == ('114', 2)
+    assert get_number_touching_index('467..114..', 6) == ('114', 1)
+    assert get_number_touching_index('467..114..', 7) == ('114', 0)
+    assert get_number_touching_index('467....114', 9) == ('114', 0)
+    assert get_number_touching_index('467....114', 8) == ('114', 1)
+    assert get_number_touching_index('467....114', 7) == ('114', 2)
+
+
+def test_get_adjacent_parts():
+    schematic = ['467..114..',
+                 '...*......',
+                 '..35..633.',
+                 '......#...',
+                 '617*......',
+                 '.....+.58.',
+                 '..592.....',
+                 '......755.',
+                 '...$.*....',
+                 '.664.598..',
+                 '......-598',
+                 ]
+    assert get_adjacent_parts(1, 3, schematic) == ['467', '35']
+    assert get_adjacent_parts(0, 0, schematic) == ['467']
+    assert get_adjacent_parts(9, 4, schematic) == ['664', '598']
+    assert get_adjacent_parts(10, 9, schematic) == ['598']
+    assert get_adjacent_parts(10, 6, schematic) == ['598', '598']
+
+def test_solve_day3_part2():
+    schematic = ['467..114..',
+                 '...*......',
+                 '..35..633.',
+                 '......#...',
+                 '617*......',
+                 '.....+.58.',
+                 '..592.....',
+                 '......755.',
+                 '...$.*....',
+                 '.664.598..',]
+    assert solve_day3_part2(schematic) == 467835
